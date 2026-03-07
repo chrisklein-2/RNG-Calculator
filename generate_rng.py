b54392f8a403f14d1095c360f32856cbbb1d7b40
chrisklein-2 <cjk5a@mtmail.MTSU.edu>
2026-03-06 18:20:05 -0600

    Added milestones for rng, added text variables in interface to avoid needing to change over several locations

--- generate_rng.py
+++ generate_rng.py
@@ -24,6 +24,15 @@ def perform_calculation(drop_rate):
 
 # Prints the results of the calculation.
 def print_results(drop_rate, kill_count, dataset):
+    milestones = {}
+    
+    # Calculate and store the probabilities at the specified milestones
+    # for future use
+    for milestone in [drop_rate//4, drop_rate//2, drop_rate, drop_rate*2, drop_rate*4]:
+        successes_at_milestone = dataset <= milestone
+        chance = np.mean(successes_at_milestone)*100
+        milestones[milestone] = f"At {milestone}/{drop_rate}: {chance:.2f}% of players received the drop!"
+    
     successes_at_kill_count = dataset <= kill_count
     chance = np.mean(successes_at_kill_count)*100
     if chance == 100:
--- inteface.py
+++ inteface.py
@@ -13,11 +13,14 @@ class Interface:
         self.kill_count = None
         self.result = None
 
-        self.project_label = tk.Label(root, text="Assess how lucky you are with RNG drops!",font=("Helvetica", 12))
+        self.rng_text = "Enter an RNG value."
+        self.kc_text = "Enter your current kill count."
+
+        self.project_label = tk.Label(root, text="Assess how lucky you are with RNG drops.", font=("Helvetica", 12))
         self.project_label.pack(pady=10)
         
         # instruction label to guide user through input process
-        self.instruction_label = tk.Label(root, text="Enter an RNG value",font=("Helvetica", 20))
+        self.instruction_label = tk.Label(root, text=self.rng_text, font=("Helvetica", 20))
         self.instruction_label.pack(pady=10)
 
         # entry field for user input
@@ -26,35 +29,36 @@ class Interface:
         self.entry.focus_set()
 
         # label to display results
-        self.result_label = tk.Label(root, text="",font=("Helvetica", 12))
+        self.result_label = tk.Label(root, text="", font=("Helvetica", 12))
         self.result_label.pack(pady=10)
 
         # button for user to submit input, also binds the Enter key to the same function for convenience
-        self.button = tk.Button(root, text="Enter", command=self.on_button_click, width = 10)
-        self.button.pack(padx=100, pady=1)
-        self.root.bind('<Return>', lambda event: self.on_button_click())
+        self.enter_button = tk.Button(root, text="Enter", command=self.on_enter_button_click, width = 10)
+        self.enter_button.pack(padx=100, pady=1)
+        self.root.bind('<Return>', lambda event: self.on_enter_button_click())
 
 
-    def on_button_click(self):
+    def on_enter_button_click(self):
 
         # first click gets rng value input, then calculates
-        if self.instruction_label.cget("text") == "Enter an RNG value":
+        if self.instruction_label.cget("text") == self.rng_text:
             try:
                 self.drop_rate = int(self.entry.get())
                 self.result = perform_calculation(self.drop_rate)
                 self.entry.delete(0, tk.END)
-                self.instruction_label.config(text="Enter your current kill count.")
+                self.instruction_label.config(text=self.kc_text)
             except ValueError:
                 self.result_label.config(text="Please enter a valid integer for the RNG value.")
                 self.entry.delete(0, tk.END)
+                
         # second click gets kill count and then calculates the final result and displays it
-        elif self.instruction_label.cget("text") == "Enter your current kill count.":
+        elif self.instruction_label.cget("text") == self.kc_text:
             try:
                 self.kill_count = int(self.entry.get())
                 final_result = print_results(self.drop_rate, self.kill_count, self.result)
                 self.result_label.config(text=final_result)
                 self.entry.delete(0, tk.END)
-                self.instruction_label.config(text="Enter an RNG value")
+                self.instruction_label.config(text=self.rng_text)
             except ValueError:
                 self.result_label.config(text="Please enter a valid integer for the kill count.")
                 self.entry.delete(0, tk.END)
